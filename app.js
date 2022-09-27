f2f19d148328c8749fe00c5f95892cf1ce74f184
khatanark <rohitkhatana16@gmail.com>
2022-09-28 01:44:05 +0530

    052 Starting Our API_ Handling GET Requests

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
 
+// Note => Top level code is executed only once when application is started.
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 // It works for get only
 app.get('/', (req, res) => {
@@ -19,9 +23,22 @@ app.get('/json', (req, res) => {
 });
 
 
+// This is the format we will send as it will be easy for the client to understand.
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+} )
+
+
 
 
 app.listen(port, () => {
     console.log('App running')
 });
 // We have passed the callback function to the listen func.
+// Note => Inside callbacks contains non-blocking code.?
