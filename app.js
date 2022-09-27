b6c15271964f4baa8bbfb858572413fe9ea3fa4f
khatanark <rohitkhatana16@gmail.com>
2022-09-28 02:11:26 +0530

    054 Responding to URL Parameters

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+// middleware express.json()  It helps to modify the incoming data
+app.use(express.json()); 
 const port = 3000;
 
 // Note => Top level code is executed only once when application is started.
@@ -23,7 +25,7 @@ app.get('/json', (req, res) => {
 });
 
 
-// This is the format we will send as it will be easy for the client to understand.
+// This is the format we will send as it will be easy for the client to understand. 
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -34,7 +36,25 @@ app.get('/api/v1/tours', (req, res) => {
     })
 } )
 
-
+//In Post request we can send data from client to server.This data ideal should be available on requets but nodesjs doesn't include on req
+// So will be using middlewares for that.SO in this case we will be using simple middleware express.json() on top. app.use(express.json());
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length-1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),
+     err => {
+        res.status(201).json(
+            {
+                status: 'success',
+                data:{
+                    tour: newTour
+                }
+            }
+        )
+    });
+    console.log(req.body);
+} );
 
 
 app.listen(port, () => {
