f6072084419cdf08b0def601713f42a68b0c0c21
khatanark <rohitkhatana16@gmail.com>
2022-09-30 00:52:11 +0530

    added config file for envs

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const express = require('express');
+
+if (process.env.NODE_ENV === 'developement'){
+    app.use(morgan('dev'))
+}
 const morgan = require('morgan'); // Logging middleware . 3rd party middleware.
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
@@ -11,25 +15,8 @@ app.use(morgan('dev'))
 app.use(express.json()); 
 app.use(express.static(`${__dirname}/public`))
 
-// This is a custom middleware. 
-app.use((req, res, next) => {
-    console.log("Hello")
-    next()
-})
 const port = 3000;
 
-//  2) ROUTE HANDLERS
-
-// This is the format we will send as it will be easy for the client to understand. 
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', addATour);
-// app.get('/api/v1/tours/:id', getATour);
-// app.patch('/api/v1/tours/:id', updateApost);
-// app.delete('/api/v1/tours/:id', deleteAPost);
-
-// 3) ROUTES
-// We will be using router middle ware.
-// These are middleWares
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // It will be our starting file.
+const dotenv = require('dotenv'); // npm install dotenv
 const app = require('./app');
-const port = 3000;
+
+dotenv.config({path: './config.env'})
+const port = process.env.PORT || 3000
+console.log(process.env)
 
 app.listen(port, () => {
     console.log('App running')
