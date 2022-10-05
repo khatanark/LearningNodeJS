dab8c0e447a8a9bb99e9816a2ffeb2b2e096b3e3
khatanark <rohitkhatana16@gmail.com>
2022-10-06 01:33:22 +0530

    103 Virtual Properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -51,8 +51,18 @@ const tourSchema = new mongoose.Schema(
             select: false // It will never be shown to client. 
         }, 
         startDates: [Date]
+    } , {
+        // We are defing the options to the schema. that is wantto show the virtual fields.
+        toJSON: {virtuals: true}, 
+        toObject: {virtuals: true}
     }
 );
+// Virtauls fields are those which are derived and are not saved in the database. 
+// We will define the tour propety on the schema , we have not used arrow func because it doesn't have its own this.
+tourSchema.virtual('durationWeeks').get(function(){
+    // this will be poining to tour documents.
+    return this.duration/7;
+});
 
 const Tour = mongoose.model('Tour', tourSchema)
 module.exports = Tour;
