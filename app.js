3717d790a1006aaf29be678d7756f2ed6db342bb
khatanark <rohitkhatana16@gmail.com>
2022-10-07 01:17:58 +0530

    113 Implementing a Global Error Handling Middleware

--- app.js
+++ app.js
@@ -20,14 +20,32 @@ const port = 3000;
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
+//IMPORTANT (Handle unhandle routes.)
 // Order of middleware is important.
-// Handle unhandle routes. This is the point where we will be defining the routes.
-// if we are here it means the request is not handled.
+// This is the point where we will be defining the handle the unhandle routes.
+// if we are here it means the request is not handled. the req cycele is not complete.
+// * means for all the verbs.
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Cant find ${req.originalUrl} on this Server!`
-    })
+    const err = new Error(`Cant find ${req.originalUrl} on this Server!`)
+    err.status = 'error'
+    err.statusCode = 404;
+    next(err);
+    // Note if next gets argument then it will assume that it is err0r and it will skip all the middlewares and will go straight to last global one,
+})
+
+
+
+// GLOBAL MIDDLEWARES.
+
+// By specifing the 4 params , the express knows this is middleware func.
+app.use((err, req, res, next) => {
+    // How do we get to know status code of errr . We will read from err object.
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error' // if not defined will pick up the default one.
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
 })
 
 
