e3de45fe1118011d56a66eef3c8c752bf2863ab3
khatanark <rohitkhatana16@gmail.com>
2022-10-08 16:58:17 +0530

    125 Creating New Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,13 @@
 // ALl the func related to authentication will be here.
 const User = require('./../models/userModel') // Import the user model.
+const catchAsync = require('./../utils/catchAsync');
 
-exports.signup = async(req, res, next) => {
+exports.signup = catchAsync(async(req, res, next) => {
     const newUser = await User.create(req.body); // THis will return a promise so , this must be async func.
-    req.status(201).json(
-        {
-            status: 'Success', 
-            data: newUser
+    res.status(201).json({
+        status: 'Success', 
+        data: {
+            tour: newUser
         }
-    );
-
-}
+    });
+});
